3d0c22267d14964f6f5fedb73321c1fa47ae99d2
mursalbekov1 <mursalbekov.merey@gmail.com>
2024-04-15 15:39:40 +0500

    firt commit

---
+++ src/main/java/com/example/convert_numbers/ConvertNumbersApplication.java
@@ -0,0 +1,13 @@
+package com.example.convert_numbers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertNumbersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConvertNumbersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/convert_numbers/ConvertNumbersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.convert_numbers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertNumbersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
